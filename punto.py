dfa7690b4f0690c3ad1e7563442896f74c460a8a
Gennadii <g.mishchevskii@gmail.com>
2015-07-14 16:07:57 +0300

    Added reserve date putting into the file

--- punto.py
+++ punto.py
@@ -2,4 +2,9 @@ import shutil
 import os.path
 from datetime import datetime
 
-shutil.copytree(os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'), os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto' + '\\' + str(datetime.now().date())))
+date = open(os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto\date.txt'), 'w')
+date.write(str(datetime.now().date()))
+date.close()
+
+shutil.copytree(os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'), \
+	os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto' + '\\' + str(datetime.now().date())))
