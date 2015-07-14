53beebfb960454b1f4f73acf24abae6402b46f15
Gennadii <g.mishchevskii@gmail.com>
2015-07-14 16:40:52 +0300

    Create punto_restore.py

---
+++ punto_restore.py
@@ -0,0 +1,12 @@
+import shutil
+import os.path
+from datetime import datetime
+
+shutil.rmtree(os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'))
+
+file_name = open(os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto\date.txt'), 'r')
+date = file_name.readline()
+file_name.close()
+
+shutil.copytree(os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto' + '\\' + str(datetime.now().date()) + '\\' + 'User Data'), \
+	os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'))
