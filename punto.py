6be8f7ce58b37b7efa61b14ae359f2cceba88be8
Gennadii <g.mishchevskii@gmail.com>
2015-09-01 18:54:34 +0300

    Fixed overriding today's data

--- punto.py
+++ punto.py
@@ -6,5 +6,12 @@ date = open(os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto\date.txt'), 'w')
 date.write(str(datetime.now().date()))
 date.close()
 
-shutil.copytree(os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'), \
+try:
+	shutil.copytree(os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'), \
+	os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto' + '\\' + str(datetime.now().date()) + '\\' + 'User Data'))
+except FileExistsError:
+	shutil.rmtree(os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto' + '\\' + \
+		str(datetime.now().date()) + '\\' + 'User Data'))
+
+	shutil.copytree(os.path.expanduser(r'~\AppData\Roaming\Yandex\Punto Switcher\User Data'), \
 	os.path.expanduser(r'~\Dropbox\Volk\Back-up\punto' + '\\' + str(datetime.now().date()) + '\\' + 'User Data'))
